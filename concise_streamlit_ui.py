46eb61d53865e31c2c7e85814470b99948c047f3
Hadiazzouni <a_azouni@esi.dz>
2023-03-25 15:29:26 +0100

    Update concise_streamlit_ui.py

--- concise_streamlit_ui.py
+++ concise_streamlit_ui.py
@@ -1,4 +1,3 @@
-
 import requests
 import streamlit as st
 
@@ -29,34 +28,14 @@ def main():
 
     concise_solution=""
 
-    search_clicked=False
-
-    api_rephrase_end_point = "https://api.concisefix.co/semparse"
-    api_end_point = "https://api.concisefix.co/answers"
-
-    # api_rephrase_end_point = "http://localhost:8080/semparse"
-    # api_end_point = "http://localhost:8080/answers"
+    api_end_point = "http://localhost:8080/answers"
 
     query = { "query": issue_text }
 
-
     if st.button("Find a fix", key="search_fix", help=None, on_click=None, args=None, kwargs=None, disabled=False):
-        clean_query = requests.post(api_rephrase_end_point, data=query).text
-        st.write("The issue description:")      
-        st.write(clean_query) 
-        search_clicked=True
-           
-            
-    # with col2:
-    #     st.button("Add context", key="add_context", help=None, on_click=None, args=None, kwargs=None, disabled=True)
-
-    # @TODO refactor and put in separate function -> display_answers() , then refer to it in the button "on_click"
-    if search_clicked:
         st.write("The most relevant solutions:")
         concise_solution = requests.post(api_end_point, data=query).text
         st.write(concise_solution) 
 
 if __name__ == '__main__':
     main()
-
-
